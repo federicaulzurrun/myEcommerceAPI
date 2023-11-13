99cace11dff5969814be9605007d93fb43cde428
Federica Ulzurrun <fzulzurrun@gmail.com>
2023-11-13 20:03:00 -0300

    products migration file

---
+++ db/migrate/20231113230226_create_product.rb
@@ -0,0 +1,12 @@
+class CreateProduct < ActiveRecord::Migration[7.0]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.string :ingridients
+      t.string :productSize
+      t.decimal :price
+
+      t.timestamps
+    end
+  end
+end
