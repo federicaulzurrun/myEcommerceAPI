bc5c24f8bedf98a4034acbf1c8734bd2418ef8c7
Federica Ulzurrun <fzulzurrun@gmail.com>
2023-11-24 10:57:18 -0300

    added models

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# db/seeds.rb
+
+# Seed Users
+User.create(name: 'John Doe', email: 'john@example.com', phone: '123-456-7890', address: '123 Main St')
+
+# Seed Products
+Product.create(name: 'Product 1', price: 19.99)
+Product.create(name: 'Product 2', price: 29.99)
+
+# Seed Orders
+Order.create(order_date: Date.today, order_status: 'Pending', user_id: 1)
+
+# Seed OrderDetails
+OrderDetail.create(quantity: 2, subtotal: 39.98, product_id: 1, order_id: 1)
+OrderDetail.create(quantity: 1, subtotal: 29.99, product_id: 2, order_id: 1)
