f1600fff44a5e2c3053bf606231cca1ed9299cb0
Federica Ulzurrun <fzulzurrun@gmail.com>
2023-11-24 11:07:10 -0300

    chenged routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:index, :show]
+      resources :products, only: [:index, :show]
+      resources :orders, only: [:index, :show]
+      resources :order_details, only: [:index, :show]
+    end
+  end
 end
