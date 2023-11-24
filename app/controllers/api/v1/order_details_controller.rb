7e6ba65b693ac626343bc14916184377e1085975
Federica Ulzurrun <fzulzurrun@gmail.com>
2023-11-24 12:14:24 -0300

    Added show and index to controllers

--- app/controllers/api/v1/order_details_controller.rb
+++ app/controllers/api/v1/order_details_controller.rb
@@ -1,2 +1,11 @@
 class Api::V1::OrderDetailsController < ApplicationController
+  def index
+    @order_details = OrderDetail.all
+    render json: @order_details
+  end
+
+  def show
+    @order_detail = User.find(params[:id])
+    render json: @order_detail
+  end
 end
